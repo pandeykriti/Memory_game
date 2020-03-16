80772c13994b81f046e2592192c7eb16d2a846d8
pandeykriti <53895407+pandeykriti@users.noreply.github.com>
2020-03-17 00:47:12 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Memory_game
-Created with CodeSandbox
---
+++ src/deck.js
@@ -0,0 +1,22 @@
+function shuffle(array){
+const _array=array.slice(0);
+for(var i=0;i<_array.length-1;i++)
+{
+var randomIndex=Math.floor(Math.random()*(i+1));
+var temp=_array[i]
+_array[i]=_array[randomIndex];
+_array[randomIndex]=temp;
+}
+return _array;
+}
+export default function initializeDeck(){
+let id=0;
+const cards=['react','reactu','redux','vue','angular','javascript','ruby','rails'].reduce((acc,type)=>{
+acc.push({
+  id:id++,
+  type
+})
+return acc
+},[])
+return shuffle(cards);
+}
